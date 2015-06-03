e957b83074b4b2f1ad5812cf9091593c5e0d05e3
Armando <apiconz@gmail.com>
2015-06-02 20:00:09 -0500

    initial commit

---
+++ app/src/main/java/pe/apiconz/android/codelab1/Calculator.java
@@ -0,0 +1,23 @@
+package pe.apiconz.android.codelab1;
+
+/**
+ * Created by Armando on 6/1/2015.
+ */
+public class Calculator {
+
+    public double sum(double a, double b){
+        return a + b;
+    }
+
+    public double substract(double a, double b){
+        return a - b;
+    }
+
+    public double divide(double a, double b){
+        return a / b;
+    }
+
+    public double multiply(double a, double b){
+        return a * b;
+    }
+}
